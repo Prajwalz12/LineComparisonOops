7ef99660889c8c8a3e3e16a4c900839fa2d643e5
Prajwal <prajwal.zurunge12@gmail.com>
2021-10-17 14:25:28 +0530

    adding using class program

---
+++ LineComparisonOops-Day8/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LineComparisonOops_Day8
+{
+    class Class1
+    {
+    }
+}
--- LineComparisonOops-Day8/Program.cs
+++ LineComparisonOops-Day8/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LineComparisonOops;
+using System;
 
 namespace LineAndComparison
 {
@@ -6,34 +7,8 @@ namespace LineAndComparison
     {
         static void Main(string[] args)
         {
-            int x1 = 4;
-            int y1 = 3;
-            int x2 = 6;
-            int y2 = 5;
-            double length1;
-            double length2;
-            int m1 = 4;
-            int n1 = 3;
-            int m2 = 6;
-            int n2 = 5;
-            length1 = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-            length2 = Math.Sqrt((m2 - m1) * (m2 - m1) + (n2 - n1) * (n2 - n1));
-            Console.WriteLine(length1);
-            Console.WriteLine(length2);
-            Console.WriteLine(length1 == length2);//Return TRUE if lines are equal otherwise false
-            if (length1 > length2)
-            {
-                Console.WriteLine("Line 1 is Greater!");
-            }
-            else if (length1 == length2)
-            {
-                Console.WriteLine("Both Lines are Equals!");
-            }
-            else
-            {
-                Console.WriteLine("Line 2 is Greater!");
-            }
-
+            CustomLines customLines = new CustomLines();
+            customLines.checkLines();
         }
     }
 }
