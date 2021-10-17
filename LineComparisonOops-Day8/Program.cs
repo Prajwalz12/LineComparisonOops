e51d0ec4fa657094af8812a975bfd90ee1a113de
Prajwal <prajwal.zurunge12@gmail.com>
2021-10-17 14:11:54 +0530

    adding calculate length program

--- LineComparisonOops-Day8/Program.cs
+++ LineComparisonOops-Day8/Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 
-namespace LineComparisonOops_Day8
+namespace LineAndComparison
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Program");
+            int x1 = 4;
+            int y1 = 3;
+            int x2 = 6;
+            int y2 = 5;
+            Console.WriteLine(Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)));
         }
     }
 }
